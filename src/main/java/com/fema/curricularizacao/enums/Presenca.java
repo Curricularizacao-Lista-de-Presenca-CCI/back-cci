0e6e09822e19947e1b72548d0945e37490268f29
Maria Vitoria Alvim Nardotto <167350735+VitoriaAlvim7@users.noreply.github.com>
2025-10-07 10:24:53 -0300

    Feat: Adicionar a enumeração Presenca com estados de presença

---
+++ src/main/java/com/fema/curricularizacao/enums/Presenca.java
@@ -0,0 +1,11 @@
+package com.fema.curricularizacao.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Presenca {
+    PRESENTE,
+    AUSENTE;
+}
