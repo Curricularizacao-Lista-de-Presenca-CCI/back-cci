4b111b901f7deebb2ec3c5e82487e50a07c63096
Maria Vitoria Alvim Nardotto <167350735+VitoriaAlvim7@users.noreply.github.com>
2025-10-07 10:23:07 -0300

    Feat: Adicionar a classe Evento com mapeamento JPA e atributos necessários

---
+++ src/main/java/com/fema/curricularizacao/models/Evento.java
@@ -0,0 +1,38 @@
+package com.fema.curricularizacao.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name="evento")
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="evento_id")
+    private Long id;
+
+    @Column(name = "titulo")
+    private String titulo;
+
+    @Column(name = "data_2")
+    private LocalDateTime data;
+
+    @Column(name = "local_2")
+    private String local;
+
+    @Column(name = "uuid_arquivo", length = 36)
+    private UUID arquivo;
+
+    @ManyToOne
+    @JoinColumn(name = "funcionario_id")
+    private Funcionario funcionario;
+}
