e69a3b0e1b4816edfca667df40a773096d1250dc
Maria Vitoria Alvim Nardotto <167350735+VitoriaAlvim7@users.noreply.github.com>
2025-10-07 10:23:16 -0300

    Feat: Adicionar a classe ListaPresença com mapeamento JPA e atributos necessários

---
+++ src/main/java/com/fema/curricularizacao/models/ListaPresença.java
@@ -0,0 +1,30 @@
+package com.fema.curricularizacao.models;
+
+import com.fema.curricularizacao.enums.Presenca;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "lista_de_presença")
+public class ListaPresença {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_aluno")
+    private String nomeAluno;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "presenca_enum")
+    private Presenca presenca;
+
+    @ManyToOne
+    @JoinColumn(name = "evento_id", nullable = false)
+    private Evento evento;
+}
