056076868e52774caaabeec58a393cf0907d0d81
YannPG <yann_pg@hotmail.com>
2025-10-28 14:39:34 -0300

    feat: implementar os Enums completos de Atuacao

--- src/main/java/com/fema/curricularizacao/enums/Atuacao.java
+++ src/main/java/com/fema/curricularizacao/enums/Atuacao.java
@@ -1,11 +1,19 @@
 package com.fema.curricularizacao.enums;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fema.curricularizacao.utils.conversao.enums.interfaces.GettersEnum;
+import com.fema.curricularizacao.utils.conversao.enums.service.DesserializadorEnum;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
-public enum Atuacao {
-    COORDENADOR,
-    PROFESSOR;
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonDeserialize(using = DesserializadorEnum.class)
+public enum Atuacao implements GettersEnum<String> {
+    COORDENADOR("C", "Coordenador"),
+    PROFESSOR("P", "Professor");
+
+    private final String valor;
+    private final String descricao;
 }
