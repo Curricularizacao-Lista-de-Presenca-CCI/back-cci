213e634a8cb71dc868eec5abb4d0a947ed518c03
YannPG <yann_pg@hotmail.com>
2025-10-28 14:39:44 -0300

    feat: implementar os Enum completos de Presenca

--- src/main/java/com/fema/curricularizacao/enums/Presenca.java
+++ src/main/java/com/fema/curricularizacao/enums/Presenca.java
@@ -1,11 +1,19 @@
 package com.fema.curricularizacao.enums;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fema.curricularizacao.utils.conversao.enums.interfaces.GettersEnum;
+import com.fema.curricularizacao.utils.conversao.enums.service.DesserializadorEnum;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
-public enum Presenca {
-    PRESENTE,
-    AUSENTE;
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonDeserialize(using = DesserializadorEnum.class)
+public enum Presenca implements GettersEnum<String> {
+    SIM("S", "Sim"),
+    NAO("N", "Não");
+
+    private final String valor;
+    private final String descricao;
 }
