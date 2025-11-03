b9fb8d3641e08336b31dd9e28c84776ff53bc531
YannPG <yann_pg@hotmail.com>
2025-11-03 00:02:14 -0300

    feat: implementar o repository de lista de presença.

--- src/main/java/com/fema/curricularizacao/repositories/ListaPresecaRepository.java
+++ src/main/java/com/fema/curricularizacao/repositories/ListaPresecaRepository.java
@@ -4,9 +4,12 @@ import com.fema.curricularizacao.models.ListaPresenca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ListaPresecaRepository extends JpaRepository<ListaPresenca, Long> {
     Optional<ListaPresenca> findById_IdEventoAndId_NomeAluno(Long idEvento, String nomeAluno);
+
+    List<ListaPresenca> findById_IdEvento(Long idEvento);
 }
